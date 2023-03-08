485e03bd0f92f7b453b7646c0075707daa463f80
Lautaro Monserrat <lautaromonserrat@yahoo.com.ar>
2023-03-08 05:54:42 -0300

    bubble sort

---
+++ src/bubble-sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int printArray(int* array, int size);
+
+int main(void) {
+	int array[8] = {3, 5, 4, 2, 1, 6, 3, 10};
+	int size = 8;
+	int tmp;
+	int swap;
+	int count = 0;
+
+	printf("Unsorted array:");
+	printArray(array, size);
+
+	do{
+		swap = 0;
+
+		for(int i = 0; i < size-1; i++) {
+			count++;
+
+			if(array[i] > array[i+1]){
+				tmp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = tmp;
+				swap = 1;
+			}
+		}
+
+	}while(swap);
+
+
+	printf("\n\nSorted array:");
+	printArray(array, size);
+	printf("\n\nTo sort the array %d iterations were perfomed", count);
+
+	return EXIT_SUCCESS;
+}
+
+
+int printArray(int* array, int size) {
+	printf("\n");
+
+	for(int i = 0; i < size; i++) {
+		printf("%d ", array[i]);
+	}
+
+	return 0;
+}
